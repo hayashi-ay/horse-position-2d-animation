cc499a9e9ae600c30dff0005c15fbca2f3124d77
hayashi-ay <go12limchangyong@gmail.com>
2025-12-22 00:03:20 +0900

    a

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const CONFIG = {
     BALL_RADIUS: 12,
     FPS: 60,
     HORSE_MARGIN_X: 50,
-    HORSE_MARGIN_Y: 5,
+    HORSE_MARGIN_Y: 15,
     VIDEO_START_TIME_OFFSET: (1 * 60) + 39,
     MEDIA_RECORDER_OPTIONS: { mimeType: 'video/webm; codecs=vp9' },
     HORSES: [
@@ -19,7 +19,7 @@ const CONFIG = {
         { number: "5", color: "yellow", textColor: "black" },
         { number: "6", color: "green", textColor: "white" },
         { number: "7", color: "orange", textColor: "white" },
-        { number: "8", color: "pink", textColor: "white" },
+        { number: "8", color: "pink", textColor: "black" },
     ]
 };
 
@@ -219,6 +219,8 @@ function animate() {
         horses[i].draw();
     }
 
+    drawRanking(interpolatedPositions);
+
     currentFrame += 1;
     if (currentFrame >= totalAnimationFrames) {
         // end recording
@@ -227,9 +229,39 @@ function animate() {
     requestAnimationFrame(animate);
 }
 
+let dress_svg = '<svg xmlns="http://www.w3.org/2000/svg" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 338 511.636"><path fill-rule="nonzero" d="M82.863 0h33.476l1.183 5.897c3.756 18.724 13.481 31.849 25.584 39.256a49.083 49.083 0 0025.052 7.241c8.668.101 17.404-2.122 25.269-6.712 12.444-7.262 22.685-20.515 27.08-39.957L221.801 0h41.118c-.903 14.518-1.825 29.036-2.709 43.555-1.126 18.444-1.209 19.841 2.899 37.925 7.994 35.167 1.566 54.641-4.904 74.238-3.971 12.03-7.96 24.114-7.96 40.198v8.382c-25.257.549-56.715.931-87.624 1.014-26.748.072-53.153-.077-74.865-.533v-8.863c0-16.084-3.989-28.168-7.96-40.198-6.469-19.597-12.898-39.071-4.904-74.238C79 63.396 78.916 61.999 77.791 43.555 76.907 29.036 75.985 14.518 75.081 0h7.782zM254.68 218.804c20.138 39.172 36.254 79.293 49.311 120.132 14.16 44.289 24.712 89.392 32.888 135.027l1.121 6.261-6.016 1.983a568.495 568.495 0 01-28.824 8.642c-7.591-41.629-16.302-83.186-27.743-124.528-11.703-42.282-26.243-84.227-45.335-125.603-1.026-2.233-3.595-3.178-5.739-2.11-2.143 1.068-3.05 3.745-2.025 5.978 18.879 40.914 33.257 82.388 44.828 124.197 11.368 41.077 20.052 82.595 27.642 124.274a620.152 620.152 0 01-43.025 9.389c-29.173 5.297-56.826 8.256-85.492 9.19V252.651c0-2.485-1.935-4.501-4.32-4.501-2.386 0-4.321 2.016-4.321 4.501v258.391c-22.129-1.24-46.336-4.047-71.392-8.596a620.76 620.76 0 01-43.026-9.389c7.572-41.66 15.819-83.185 26.762-124.31 11.129-41.823 25.057-83.291 43.914-124.161 1.025-2.233.118-4.91-2.025-5.978-2.143-1.068-4.713-.123-5.738 2.11-19.112 41.421-33.206 83.372-44.454 125.639-10.988 41.294-19.254 82.844-26.831 124.492a568.152 568.152 0 01-28.823-8.642L0 480.224l1.122-6.261c8.177-45.635 18.728-90.738 32.887-135.027 13.005-40.673 29.044-80.636 49.068-119.657 22.512.532 50.807.708 79.544.631 32.845-.089 66.174-.508 92.059-1.106z"/></svg>'
+
+function drawRanking(interpolatedPositions) {
+    const ranking = interpolatedPositions
+        .map((pos, i) => ({ index: i, x: pos.x }))
+        .sort((a, b) => b.x - a.x)
+        .slice(0, 3);
+
+    const boxSize = 20;
+    const startX = 10;
+    const startY = 10;
+    const gap = 10;
+
+    ranking.forEach((item, i) => {
+        const horse = horses[item.index];
+        const y = startY + i * (boxSize + gap);
+
+        ctx.fillStyle = horse.color;
+        ctx.fillRect(startX, y, boxSize, boxSize);
+
+        ctx.fillStyle = horse.textColor;
+        ctx.font = "bold 12px Arial";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillText(horse.number, startX + boxSize / 2, y + boxSize / 2);
+    });
+}
+
 let mediaRecorder;
 let recordedChunks = [];
 let isRecording = false;
+
+animate();
 
 recordButton.addEventListener('click', () => {
     // for just debugging.
